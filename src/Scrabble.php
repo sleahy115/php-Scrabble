a406c65239721ea2ebe9a3d96f6d96049c7ed51d
Leah Sherrell and Sarah Leahy <sarah@>
2017-02-16 13:49:59 -0800

    pass fifth test case

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -11,6 +11,17 @@
     $total = $score*1;
     $score = preg_match_all('/[D, G]/i',$word);
     $total += $score*2;
+    $score = preg_match_all('/[B, C, M, P]/i',$word);
+    $total += $score*3;
+    $score = preg_match_all('/[F, H, V, W, Y]/i',$word);
+    $total += $score*4;
+    $score = preg_match_all('/[K]/i',$word);
+    $total += $score*5;
+    $score = preg_match_all('/[J, X]/i',$word);
+    $total += $score*8;
+    $score = preg_match_all('/[Q, Z]/i',$word);
+    $total += $score*10;
+
 
     return $total;
     }
--- tests/ScrabbleTest.php
+++ tests/ScrabbleTest.php
@@ -29,6 +29,7 @@ require_once "src/Scrabble.php";
           $this->AssertEquals(2, $result);
 
         }
+
         function test_go()
         {
           $test_arrays = new Scrabble;
@@ -42,6 +43,36 @@ require_once "src/Scrabble.php";
           $this->AssertEquals(3, $result);
 
         }
+
+        function test_food()
+        {
+          $test_arrays = new Scrabble;
+          //Arrange
+          $input = "food";
+
+          $test_arrays->wordScore($input);
+          //Act
+          $result = $test_arrays->wordScore($input);
+          //Assert
+          $this->AssertEquals(8, $result);
+
+        }
+
+        function test_queen()
+        {
+          $test_arrays = new Scrabble;
+          //Arrange
+          $input = "queen";
+
+          $test_arrays->wordScore($input);
+          //Act
+          $result = $test_arrays->wordScore($input);
+          //Assert
+          $this->AssertEquals(14, $result);
+
+        }
+
+
 
 }
  ?>
